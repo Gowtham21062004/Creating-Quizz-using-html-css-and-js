c4117a70da7fb24b4d1341dfa31088cafe4eaf65
Gowtham21062004 <142236401+Gowtham21062004@users.noreply.github.com>
2024-07-22 17:20:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// function red(){
+//     document.getElementsByClassName("red")[0].style.backgroundColor="red";
+// }
+// function green(){
+//     document.getElementsByClassName("green")[0].style.backgroundColor="green";
+// }
+function red(){
+    document.getElementsByClassName("para")[0].style.backgroundColor="red";
+}
+function red1(){
+    document.getElementsByClassName("para")[1].style.backgroundColor="red";
+}
+function red2(){
+    document.getElementsByClassName("para")[2].style.backgroundColor="red";
+}
+function green(){
+    document.getElementsByClassName("para")[3].style.backgroundColor="green";
+}
